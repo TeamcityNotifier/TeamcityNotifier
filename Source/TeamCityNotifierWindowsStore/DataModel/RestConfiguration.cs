57a38c15721e3873463838362b46376c38e10a08
Tobias Röthlin <tobias.roethlin@bbv.ch>
2013-11-06 17:18:40 +0100

    Added UI changes

---
+++ Source/TeamCityNotifierWindowsStore/DataModel/RestConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace TeamCityNotifierWindowsStore.Data
+{
+    using TeamcityNotifier;
+
+    public class RestConfiguration : IRestConfiguration
+    {
+        public RestConfiguration(string baseUrl, string userName, string password)
+        {
+            this.BaseUrl = baseUrl;
+            this.UserName = userName;
+            this.Password = password;
+        }
+
+        public string BaseUrl { get; private set; }
+
+        public string UserName { get; private set; }
+
+        public string Password { get; private set; }
+    }
+}
