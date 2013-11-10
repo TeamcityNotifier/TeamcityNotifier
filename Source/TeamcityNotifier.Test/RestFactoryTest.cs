1fa1adad559f3e4399a7207fcc1802ed17b976aa
mas.steiner <119baracuda@gmail.com>
2013-11-10 22:43:33 +0100

    add additional unit tests

--- Source/TeamcityNotifier.Test/RestFactoryTest.cs
+++ Source/TeamcityNotifier.Test/RestFactoryTest.cs
@@ -22,9 +22,10 @@
         private IList<IRestConfiguration> restConfigurations;
 
         private IWrapperFactory mockWrapperFactory;
+        private IXmlSerializer mockXmlSerializerProject;
 
-        private project project;
-        private projects1 project1;
+        private project fakeProject;
+        private projects1 fakeProject1;
         private IRestConfiguration configuration1;
         private IRestConfiguration configuration2;
 
@@ -34,17 +35,15 @@
             this.restConfigurations = new List<IRestConfiguration>();
             this.mockWrapperFactory = A.Fake<IWrapperFactory>();
 
-            project = new project();
-            project1 = new projects1();
-
-            var mockXmlSerializerProject = A.Fake<IXmlSerializer>();
-            A.CallTo(() => mockXmlSerializerProject.Deserialize(A<IStringReader>.Ignored)).Returns(project);
+            fakeProject = new project();
+            mockXmlSerializerProject = A.Fake<IXmlSerializer>();
+            A.CallTo(() => mockXmlSerializerProject.Deserialize(A<IStringReader>.Ignored)).Returns(fakeProject);
             A.CallTo(() => mockWrapperFactory.CreateXmlSerializer(typeof(project))).Returns(mockXmlSerializerProject);
 
+            fakeProject1 = new projects1();
             var mockXmlSerializerProject1 = A.Fake<IXmlSerializer>();
-            A.CallTo(() => mockXmlSerializerProject1.Deserialize(A<IStringReader>.Ignored)).Returns(project1);
+            A.CallTo(() => mockXmlSerializerProject1.Deserialize(A<IStringReader>.Ignored)).Returns(fakeProject1);
             A.CallTo(() => mockWrapperFactory.CreateXmlSerializer(typeof(projects1))).Returns(mockXmlSerializerProject1);
-
 
             this.configuration1 = A.Fake<IRestConfiguration>();
             A.CallTo(() => this.configuration1.BaseUrl).Returns("url1");
@@ -59,8 +58,35 @@
             A.CallTo(() => this.configuration2.Password).Returns("password2");
             restConfigurations.Add(this.configuration2);
 
-            
+
             this.testee = new RestFactory(this.restConfigurations, this.mockWrapperFactory);
+        }
+
+        [Test]
+        public void CreateServer_WhenNoConfigurationExists_NoServerIsReturned()
+        {
+            this.restConfigurations.Clear();
+
+            var servers = this.testee.CreateServers();
+
+            servers.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void CreateServer_WhenAServerIsConfigured_ThenReturnTheConfiguredServer()
+        {
+            this.restConfigurations.Clear();
+            this.restConfigurations.Add(this.configuration1);
+            var mockUri = A.Fake<IUri>();
+
+            A.CallTo(() => this.mockWrapperFactory.CreateUri(A<string>.Ignored)).Returns(mockUri);
+
+            var server = this.testee.CreateServers().FirstOrDefault();
+
+            server.Should().NotBeNull("No server returned");
+            server.Uri.Should().Be(mockUri, "Wrong uri is configured");
+            server.UserName.Should().Be("user1", "Wrong username is configured");
+            server.Password.Should().Be("password1", "Wrong password is configured");
         }
 
         [Test]
@@ -72,17 +98,47 @@
         }
 
         [Test]
-        public void CreateServer_WhenAServerisConfigured_ThenReturnTheConfiguredServer()
+        public void CreateProjects_WhenNoProjectIsAvaiable_NoProjectIsReturned()
         {
-            restConfigurations.Clear();
-            restConfigurations.Add(this.configuration1);
+            var mockServer = A.Fake<IServer>();
 
-            var server = this.testee.CreateServers().FirstOrDefault();
+            var projects = this.testee.CreateProjects(mockServer);
 
-            server.Should().NotBeNull("No server returned");
-            server.UserName.Should().Be("user1", "Wrong username is configured");
-            server.Password.Should().Be("password1", "Wrong password is configured");
+            var projectCount = projects.Count();
+            projectCount.Should().Be(0, string.Format("No server is available but {0} server(s) are returend.", projectCount));
         }
+
+        [Test]
+        public void CreateProjects_WhenOneProjectIsAvaiable_ThisProjectIsReturned()
+        {
+            var projectName = "testProject";
+            var projectDescription = "projectDescription";
+            this.fakeProject.name = projectName;
+            this.fakeProject.description = projectDescription;
+
+            this.fakeProject1.project.Add(new projectref());
+            var mockServer = A.Fake<IServer>();
+
+            var projects = this.testee.CreateProjects(mockServer);
+
+            var projectCount = projects.Count();
+            projects.FirstOrDefault().Name.Should().Be(projectName, "The project name is wrong.");
+            projects.FirstOrDefault().Description.Should().Be(projectDescription, "The project description is wrong.");
+        }   
         
+        [Test]
+        public void CreateProjects_WhenTwoProjectsAreAvaiable_TwoProjectsAreReturned()
+        {
+            this.fakeProject1.project.Add(new projectref());
+            this.fakeProject1.project.Add(new projectref());
+            var mockServer = A.Fake<IServer>();
+
+            var projects = this.testee.CreateProjects(mockServer);
+
+            var projectCount = projects.Count();
+            projectCount.Should().Be(2, string.Format("Two projects are available but {0} are retuned.", projectCount));
+
+        }
+
     }
 }
--- Source/TeamcityNotifier/RestFactory.cs
+++ Source/TeamcityNotifier/RestFactory.cs
@@ -2,7 +2,7 @@
 {
     using System.Collections.Generic;
 
-    using TeamcityNotifier.Wrapper;
+    using Wrapper;
 
     using xsdtest;
 
