65c970f008891f597386870824b0da3225502e7c
massteiner <marcel.steiner@bbv.ch>
2013-11-13 09:03:35 +0100

    update unit tests

--- Source/TeamcityNotifier.Test/ServerTest.cs
+++ Source/TeamcityNotifier.Test/ServerTest.cs
@@ -1,7 +1,36 @@
 ﻿namespace TeamcityNotifier.Test
 {
+    using FakeItEasy;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    using TeamcityNotifier.Wrapper;
+
     public class ServerTest
     {
-         
+        private IServer testee;
+
+        private IUri mockUri;
+
+        private string userName = "userName";
+
+        private string password = "password";
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mockUri = A.Fake<IUri>();
+            this.testee = new Server(this.mockUri, userName, password);
+        }
+
+        [Test]
+        public void CreateNewServer_WhenParametersAreSet_ThePropertiesAreSetCorrectliy()
+        {
+            this.testee.Uri.Should().Be(this.mockUri);
+            this.testee.UserName.Should().Be(userName);
+            this.testee.Password.Should().Be(password);
+        } 
     }
 }
--- Source/TeamcityNotifier.Test/ServiceTest.cs
+++ Source/TeamcityNotifier.Test/ServiceTest.cs
@@ -1,7 +1,30 @@
 ﻿namespace TeamcityNotifier.Test
 {
+    using FakeItEasy;
+
+    using NUnit.Framework;
+
     public class ServiceTest
     {
-         
+        private IService testee;
+
+        private IFactory mockFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mockFactory = A.Fake<IFactory>();
+
+            this.testee = new Service(this.mockFactory);
+        }
+
+        [Test]
+        public void Test()
+        {
+            this.testee.GetServers();
+
+            A.CallTo(()=> this.mockFactory.CreateServers()).MustHaveHappened();
+        }
+
     }
 }
