61b1f2699ea5a4631305278de1b96c3eca7bcec6
Heinrich Muralt <h_muralt@bluewin.ch>
2013-11-15 11:31:35 +0100

    Updated gitignore and build command line script

--- Scripts/build.cmd
+++ Scripts/build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Unrestricted -Command .\psake.ps1 -properties @{"publish"=$false;"teamcity"=$false;};
+powershell -ExecutionPolicy Unrestricted -Command .\psake.ps1
 pause
